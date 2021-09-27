193ca9eb947099b9a0c5f10ffda9d5c1595ed891
Sunny Donaldson <sunnydonaldson1@gmail.com>
2021-09-27 15:02:01 +0100

    allowing charges

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,13 +1,14 @@
 class Oystercard
+  MAXIMUMLIMIT = 100
   def initialize
     @balance = 0
   end
 
   def add(amount)
-    @balance + amount > 100 ? (fail "breaches maximum amount") : @balance += amount 
+    @balance + amount > MAXIMUMLIMIT ? (fail "breaches maximum amount") : @balance += amount 
   end
-end
 
-# In order to protect my money
-# As a customer
-# I don't want to put too much money on my card
+  def charge(fare)
+    @balance - fare < 0 ? (fail "breaches minimum amount") : @balance -= fare 
+  end 
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -14,7 +14,20 @@ describe Oystercard do
     end
 
     it 'has limit of 100£' do
-      expect{ subject.add(101)}.to raise_error 'breaches maximum amount'
+      expect{ subject.add(Oystercard::MAXIMUMLIMIT + 1)}.to raise_error 'breaches maximum amount'
+    end
+  end
+
+  describe "#charge" do
+    it "deducts fare from card" do
+      subject.add(20)
+      subject.charge(10)
+      expect(subject.instance_variable_get(:@balance)).to eq 10
+    end
+
+    it "makes sure you have the minimum amount for a journey" do
+      subject.add(20)
+      expect{ subject.charge(30)}.to raise_error 'breaches minimum amount'
     end
   end
 end
